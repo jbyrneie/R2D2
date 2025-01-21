1b56a025ece60b956e655bde4a03fd0724575008
jbyrneie <jbyrne@glgroup.com>
2025-01-21 18:12:00 +0000

    use command line params

--- brs.js
+++ brs.js
@@ -1,3 +1,4 @@
+const commandLineArgs = require('command-line-args')
 const brs = require('./lib/book_tee_time')
 const whatsApp = require('./lib/messaging')
 const moment = require('moment')
@@ -17,7 +18,7 @@ const player2UID = profile.config.whichProfile.player2UID
 const player3UID = profile.config.whichProfile.player3UID
 const player4UID = profile.config.whichProfile.player4UID
 
-console.log(`Booking dateRequired: ${dateRequired} teeTime: ${teeTime} dateComesAlive: ${dateComesAlive}`)
+const players = { JACK: '64', PHILIP: '158', PADDY: '1239', CORMAC: '666', COLM: '566', GUEST: '-2' }
 
 _seconds_to_time = function (seconds) {
   let measuredTime = new Date(null)
@@ -26,30 +27,29 @@ _seconds_to_time = function (seconds) {
 }
 
 _get_time_difference = function (dateComesAlive) {
-  //console.log('_get_time_difference: ', dateComesAlive)
+  console.log('_get_time_difference: ', dateComesAlive)
   const comesAlive = moment(dateComesAlive, 'YYYY-MM-DD HH:mm:ss')
   const now = moment(new Date())
   return moment.duration(comesAlive.diff(now)).asSeconds()
 }
 
-_brs_recursive = async function (teeTime, data) {
-  //console.log(`_brs_recursive teeTime:${teeTime}`)
-
+_brs_recursive = async function (options, data) {
+  console.log(`options: ${JSON.stringify(options)}`)
+  let teeTime = options.teeTime
   let response = await brs.book_the_tee_time_recursive(
-    dateRequired,
+    options.dateRequired,
     teeTime,
-    dateComesAlive,
+    options.dateComesAlive,
     data,
-    player1UID,
-    player2UID,
-    player3UID,
-    player4UID
+    options.golfers[0] ? players[options.golfers[0].toUpperCase()] : null,
+    options.golfers[1] ? players[options.golfers[1].toUpperCase()] : null,
+    options.golfers[2] ? players[options.golfers[2].toUpperCase()] : null,
+    options.golfers[3] ? players[options.golfers[3].toUpperCase()] : null
   )
 
   switch (response.status) {
     case status.NOT_LIVE_YET:
-      let timeDiff = _get_time_difference(dateComesAlive)
-      //console.log('timeDiff: ', timeDiff)
+      let timeDiff = _get_time_difference(options.dateComesAlive)
       let sleepTime = 0
 
       if (timeDiff > 90) sleeptime = 60000
@@ -58,7 +58,7 @@ _brs_recursive = async function (teeTime, data) {
       else if (timeDiff >= 1) sleeptime = 1000
       else if (timeDiff >= 0.4) sleeptime = 200
       else {
-        console.log(`Date comes alive is in the past ${dateComesAlive}`)
+        console.log(`Date comes alive is in the past ${options.dateComesAlive}`)
         sleeptime = 100
         notLiveRetries++
       }
@@ -70,21 +70,21 @@ _brs_recursive = async function (teeTime, data) {
           } seconds as time_diff is: ${_seconds_to_time(timeDiff >= 0 ? timeDiff : -timeDiff)}\n\n`
         )
         setTimeout(async function () {
-          await _brs_recursive(teeTime, data)
+          await _brs_recursive(options, data)
         }, sleeptime)
       } else console.log('Giving up.... exceeded notLiveRetries.... goodbye')
       break
     case status.BOOKED:
-      console.log(`A Tee-time for ${teeTime} has been booked ${dateRequired}`)
+      console.log(`A Tee-time for ${teeTime} has been booked ${options.dateRequired}`)
       break
     case status.UNAVAILABLE:
-      console.log(`A Tee-time for ${teeTime} is unavailable on ${dateRequired}`)
+      console.log(`A Tee-time for ${teeTime} is unavailable on ${options.dateRequired}`)
       break
     case status.ALREADY_BOOKED_BY_YOU:
-      console.log(`What are doing, you have already booked a Tee-time for ${teeTime} on ${dateRequired}`)
+      console.log(`What are doing, you have already booked a Tee-time for ${teeTime} on ${options.dateRequired}`)
       break
     case status.ALREADY_BOOKED:
-      console.log(`Bummer the Tee-time for ${teeTime} for ${dateRequired} is already taken`)
+      console.log(`Bummer the Tee-time for ${teeTime} for ${options.dateRequired} is already taken`)
       console.log(`We should try the next one retries: ${retries}\n\n`)
 
       retries++
@@ -104,12 +104,29 @@ _brs_recursive = async function (teeTime, data) {
 }
 
 start = async function () {
-  //console.log('lets go...')
+  const optionDefinitions = [
+    { name: 'userName', alias: 'u', type: String },
+    { name: 'password', alias: 'p', type: String },
+    { name: 'teeTime', alias: 't', type: String },
+    { name: 'dateRequired', alias: 'd', type: String },
+    { name: 'dateComesAlive', alias: 'a', type: String },
+    { name: 'golfers', alias: 'g', type: String, multiple: true, defaultOption: true }
+  ]
 
-  const response = await brs.login(login, password)
+  if (process.argv.length < 15) {
+    console.log(
+      '\n\nusage: node brs.js -u <BRS_NUMBER> -p <PASSWORD> -a <YYYY-MM-DD:HH:MM> -d <YYYY-MM-DD:HH:MM> -g <PLAYERS> -t <HH:MM>\n\n'
+    )
+    return
+  }
+
+  const options = commandLineArgs(optionDefinitions)
+
+  console.log(`options: ${JSON.stringify(options)}`)
+
+  const response = await brs.login(options.userName, options.password)
   if (response.status == status.LOGGED_IN) {
-    //console.log(`Logged In, ready to go.....  \n\n`)
-    await _brs_recursive(teeTime, response.data)
+    await _brs_recursive(options, response.data)
   } else console.log('Login failed..... goodbye\n\n')
 }
 
